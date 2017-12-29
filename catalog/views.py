caecac4f83fc86920b519c99b04f55185ff6cc8d
Hamish Willee <hamishwillee@gmail.com>
2017-12-29 21:52:08 +1100

    Update reqs to django 2 and make build

--- catalog/views.py
+++ catalog/views.py
@@ -90,7 +90,7 @@ class LoanedBooksAllListView(PermissionRequiredMixin,generic.ListView):
 
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 import datetime
 from django.contrib.auth.decorators import permission_required
 
