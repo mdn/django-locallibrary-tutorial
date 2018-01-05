a30065ef524b61db3ec5c239f0967594ac4ceead
Hamish Willee <hamishwillee@gmail.com>
2018-01-04 23:00:03 -0800

    Make author model ordered in order to squish test warning about pagination

--- catalog/models.py
+++ catalog/models.py
@@ -118,6 +118,9 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('died', null=True, blank=True)
+
+    class Meta:
+        ordering = ["last_name","first_name"]
     
     def get_absolute_url(self):
         """
