3b981a0ae5c546ba7e73e5171bf7c878e7df2c50
Hamish Willee <hamishwillee@gmail.com>
2018-02-09 14:56:34 +1100

    Fix comment for Book::genre field

--- catalog/models.py
+++ catalog/models.py
@@ -42,8 +42,8 @@ class Book(models.Model):
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
     isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
-      # ManyToManyField used because Subject can contain many books. Books can cover many subjects.
-      # Subject declared as an object because it has already been defined.
+      # ManyToManyField used because a genre can contain many books and a Book can cover many genres.
+      # Genre class has already been defined so we can specify the object above.
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
       
     def display_genre(self):
