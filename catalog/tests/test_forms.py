9100c9756e3af5f0ddc564b6803f9e3f0cdb051b
Hamish Willee <hamishwillee@gmail.com>
2018-11-11 18:57:52 +1100

    update test code to match the docs

--- catalog/tests/test_forms.py
+++ catalog/tests/test_forms.py
@@ -11,29 +11,25 @@ class RenewBookFormTest(TestCase):
     def test_renew_form_date_in_past(self):
         """Test form is invalid if renewal_date is before today."""
         date = datetime.date.today() - datetime.timedelta(days=1)
-        form_data = {'renewal_date': date}
-        form = RenewBookForm(data=form_data)
+        form = RenewBookForm(data={'renewal_date': date})
         self.assertFalse(form.is_valid())
 
     def test_renew_form_date_too_far_in_future(self):
         """Test form is invalid if renewal_date more than 4 weeks from today."""
         date = datetime.date.today() + datetime.timedelta(weeks=4) + datetime.timedelta(days=1)
-        form_data = {'renewal_date': date}
-        form = RenewBookForm(data=form_data)
+        form = RenewBookForm(data={'renewal_date': date})
         self.assertFalse(form.is_valid())
 
     def test_renew_form_date_today(self):
         """Test form is valid if renewal_date is today."""
         date = datetime.date.today()
-        form_data = {'renewal_date': date}
-        form = RenewBookForm(data=form_data)
+        form = RenewBookForm(data={'renewal_date': date})
         self.assertTrue(form.is_valid())
         
     def test_renew_form_date_max(self):
         """Test form is valid if renewal_date is within 4 weeks."""
         date = timezone.now() + datetime.timedelta(weeks=4)
-        form_data = {'renewal_date': date}
-        form = RenewBookForm(data=form_data)
+        form = RenewBookForm(data={'renewal_date': date})
         self.assertTrue(form.is_valid())
         
         
