f0bad6e5527f85af5dba596b06f164a58a2482a5
AlekseiMarinichenko <aleksey.marinichenko@gmail.com>
2018-11-14 12:55:03 +0200

    Fix display_genre.short_description = 'Genre'

--- catalog/models.py
+++ catalog/models.py
@@ -38,8 +38,8 @@ class Book(models.Model):
       
     def display_genre(self):
         """Creates a string for the Genre. This is required to display genre in Admin."""
-        return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
-        display_genre.short_description = 'Genre'
+        return ', '.join([ genre.name for genre in self.genre.all()[:3] ])    
+    display_genre.short_description = 'Genre'
     
     
     def get_absolute_url(self):
