c0f1c024a6f88904ea0e745c1a97a01c4b46eeed
t.rapp <tobias.rapp@irix.ch>
2020-10-09 10:45:32 +0200

    week_target_description und week_target_comment in BookAdmin hinzugefügt

--- catalog/admin.py
+++ catalog/admin.py
@@ -47,7 +47,7 @@ class BookAdmin(admin.ModelAdmin):
      - fields to be displayed in list view (list_display)
      - adds inline addition of book instances in book view (inlines)
     """
-    list_display = ('title', 'author', 'display_genre')
+    list_display = ('title', 'author', 'display_genre', 'week_target_description', 'week_target_comment')
     inlines = [BooksInstanceInline]
 
 
