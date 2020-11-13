63f30111f3127bb877eaffe274da3007536973d6
t.rapp <tobias.rapp@irix.ch>
2020-11-13 18:37:05 +0100

    associated_email_received_account hinzugefügt

--- catalog/models.py
+++ catalog/models.py
@@ -31,7 +31,9 @@ class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
     associated_email_received_account = models.CharField(max_length=1000, default='')
+    associated_email_subject = models.CharField(max_length=1000, default='')
     filepath_for_readiness_enhancement = models.CharField(max_length=2000, default='')
+    hyperlink_for_readiness_enhancement = models.CharField(max_length=2000, default='')
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     # Foreign Key used because book can only have one author, but authors can have multiple books
     # Author as a string rather than object because it hasn't been declared yet in file.
