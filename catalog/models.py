8f405be35876050014e490ff279eff1ad789f10f
t.rapp <tobias.rapp@irix.ch>
2020-11-14 17:39:19 +0100

    Neues model hinzu

--- catalog/models.py
+++ catalog/models.py
@@ -220,3 +220,20 @@ class T_Conflict(models.Model):
 
     class Meta:
         db_table = 't_conflict'
+
+
+
+class T_Memorization_Package_Memory_Palace_Technique(models.Model):
+#    t_memory_palace_type_id = models.ForeignKey('T_Memory_Palace_Type', on_delete=models.SET_NULL, null=True)
+    t_week_target_id = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    t_workpackage_relevantinformation_tobememorized_id = models.ForeignKey('BookInstance', on_delete=models.SET_NULL, null=True)
+    t_information_item_tobeoperationalized_id = models.ForeignKey('T_Information_Item_Tobeoperationalized', on_delete=models.SET_NULL, null=True)
+#    t_conflict_strategy_category_measure_id = models.ForeignKey('T_Conflict_Strategy_Category_Measure', on_delete=models.SET_NULL, null=True)
+    t_calendar_id = models.ForeignKey('T_Calendar', on_delete=models.SET_NULL, null=True)
+    created_datetime = models.DateTimeField(null=True, blank=True)
+    updated_datetime = models.DateTimeField(null=True, blank=True)
+    memorization_package_title = models.CharField(max_length=255, default='')
+    memorization_package_is_active = models.IntegerField(null=True, default=None)
+
+    class Meta:
+        db_table = 't_memorization_package_memory_palace_technique'
