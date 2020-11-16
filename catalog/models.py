94105e043c30e8e0024f1d488f54f6c9ff5d24ea
t.rapp <tobias.rapp@irix.ch>
2020-11-16 16:42:43 +0100

    Anpassungen html Attribute

--- catalog/models.py
+++ catalog/models.py
@@ -52,6 +52,7 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('died', null=True, blank=True)
+
     memorizable_week_target = models.CharField(max_length=4000, default='')
     week_target_comment = models.CharField(max_length=4000, default='')
     
