577dd1ebd1a607c7015c12f6f0f9ecf35a9e14db
t.rapp <tobias.rapp@irix.ch>
2020-11-21 15:01:59 +0100

    Filtering on Foreign Key hinzu

--- catalog/templatetags/app_filters.py
+++ catalog/templatetags/app_filters.py
@@ -18,3 +18,12 @@ def get_due_date_string(value):
         return "Tomorrow"
     elif delta.days > 1:
         return "In %s days" % delta.days
+
+
+from ..models import T_Memory_Palace_Type
+
+#Filtering on foreign key
+@register.filter(name='get_dictionary_value_from_variable2')
+def get_dictionary_value_from_variable2(key):
+    t = T_Memory_Palace_Type.objects.filter(id=key)
+    return t
