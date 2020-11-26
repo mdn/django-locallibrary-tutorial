83f4b1059673c14b671b63e67680406961529d9b
t.rapp <tobias.rapp@irix.ch>
2020-11-26 18:56:43 +0100

    LoginRequiredMixin als Argument hinzu

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -31,269 +32,268 @@ def index(request):
 from django.views import generic
 
 
-class BookListView(generic.ListView):
+class BookListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of books."""
     model = Book
     paginate_by = 10
 
-class BookDetailView(generic.DetailView):
+class BookDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a book."""
     model = Book
 
 
-class T_Workpackage_Actual_Duration_TimeseriesListView(generic.ListView):
+class T_Workpackage_Actual_Duration_TimeseriesListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_workpackages_actual_duration_timeseries."""
     model = T_Workpackage_Actual_Duration_Timeseries
     paginate_by = 10
 
-class T_Workpackage_Actual_Duration_TimeseriesDetailView(generic.DetailView):
+class T_Workpackage_Actual_Duration_TimeseriesDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_workpackage_actual_duration_timeseries."""
     model = T_Workpackage_Actual_Duration_Timeseries
 
 
-class AuthorListView(generic.ListView):
+class AuthorListView(LoginRequiredMixin, generic.ListView):
     """Generic class-based list view for a list of authors."""
     model = Author
     paginate_by = 10
 
-class AuthorDetailView(generic.DetailView):
+class AuthorDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for an author."""
     model = Author
 
 
-class T_Wt_Is_Excluded_From_Dt_Mp_Assignment_On_Weekday_TimeseriesListView(generic.ListView):
+class T_Wt_Is_Excluded_From_Dt_Mp_Assignment_On_Weekday_TimeseriesListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based list view for a list of t_wt_is_excluded_from_dt_mp_assignment_on_weekdays_timeseries."""
     model = T_Wt_Is_Excluded_From_Dt_Mp_Assignment_On_Weekday_Timeseries
     paginate_by = 10
 
-class T_Wt_Is_Excluded_From_Dt_Mp_Assignment_On_Weekday_TimeseriesDetailView(generic.DetailView):
+class T_Wt_Is_Excluded_From_Dt_Mp_Assignment_On_Weekday_TimeseriesDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_wt_is_excluded_from_dt_mp_assignment_on_weekday_timeseries."""
     model = T_Wt_Is_Excluded_From_Dt_Mp_Assignment_On_Weekday_Timeseries
 
 
-class T_Day_Target_Sequence_TimeseriesListView(generic.ListView):
+class T_Day_Target_Sequence_TimeseriesListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based list view for a list of t_day_targets_sequence_timeseries."""
     model = T_Day_Target_Sequence_Timeseries
     paginate_by = 10
 
-class T_Day_Target_Sequence_TimeseriesDetailView(generic.DetailView):
+class T_Day_Target_Sequence_TimeseriesDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_day_target_sequence_timeseries."""
     model = T_Day_Target_Sequence_Timeseries
 
 
-class T_CalendarListView(generic.ListView):
+class T_CalendarListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_calendars."""
     model = T_Calendar
     paginate_by = 10
 
-class T_CalendarDetailView(generic.DetailView):
+class T_CalendarDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_calendar."""
     model = T_Calendar
 
 
-class T_ConflictListView(generic.ListView):
+class T_ConflictListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_conflicts."""
     model = T_Conflict
     paginate_by = 10
 
-class T_ConflictDetailView(generic.DetailView):
+class T_ConflictDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_conflict."""
     model = T_Conflict
 
 
-class T_Ausatemmuskulatur_Isnot_Entspannt_Dueto_StrategyrefinemtListView(generic.ListView):
+class T_Ausatemmuskulatur_Isnot_Entspannt_Dueto_StrategyrefinemtListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_ausatemmuskulatur_isnot_entspannt_dueto_strategyrefinemt."""
     model = T_Ausatemmuskulatur_Isnot_Entspannt_Dueto_Strategyrefinemt
     paginate_by = 10
 
-class T_Ausatemmuskulatur_Isnot_Entspannt_Dueto_StrategyrefinemtDetailView(generic.DetailView):
+class T_Ausatemmuskulatur_Isnot_Entspannt_Dueto_StrategyrefinemtDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_ausatemmuskulatur_isnot_entspannt_dueto_strategyrefinemt."""
     model = T_Ausatemmuskulatur_Isnot_Entspannt_Dueto_Strategyrefinemt
 
 
-class T_Ausatemmuskulatur_Strategyrefinement_Conflict_PhaseListView(generic.ListView):
+class T_Ausatemmuskulatur_Strategyrefinement_Conflict_PhaseListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_ausatemmuskulatur_strategyrefinement_conflict_phases."""
     model = T_Ausatemmuskulatur_Strategyrefinement_Conflict_Phase
     paginate_by = 10
 
-class T_Ausatemmuskulatur_Strategyrefinement_Conflict_PhaseDetailView(generic.DetailView):
+class T_Ausatemmuskulatur_Strategyrefinement_Conflict_PhaseDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_ausatemmuskulatur_strategyrefinement_conflict_phase."""
     model = T_Ausatemmuskulatur_Strategyrefinement_Conflict_Phase
 
 
-class T_Conflict_Strategy_CategoryListView(generic.ListView):
+class T_Conflict_Strategy_CategoryListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_conflict_strategy_categorys."""
     model = T_Conflict_Strategy_Category
     paginate_by = 10
 
-class T_Conflict_Strategy_CategoryDetailView(generic.DetailView):
+class T_Conflict_Strategy_CategoryDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_conflict_strategy_category."""
     model = T_Conflict_Strategy_Category
 
 
-class T_Conflict_Strategy_Category_MeasureListView(generic.ListView):
+class T_Conflict_Strategy_Category_MeasureListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_conflict_strategy_category_measures."""
     model = T_Conflict_Strategy_Category_Measure
     paginate_by = 10
 
-class T_Conflict_Strategy_Category_MeasureDetailView(generic.DetailView):
+class T_Conflict_Strategy_Category_MeasureDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_conflict_strategy_category_measure."""
     model = T_Conflict_Strategy_Category_Measure
 
 
-class T_Information_Item_TobeoperationalizedListView(generic.ListView):
+class T_Information_Item_TobeoperationalizedListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_information_items_tobeoperationalized."""
     model = T_Information_Item_Tobeoperationalized
     paginate_by = 10
 
-class T_Information_Item_TobeoperationalizedDetailView(generic.DetailView):
+class T_Information_Item_TobeoperationalizedDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_information_item_tobeoperationalized."""
     model = T_Information_Item_Tobeoperationalized
 
 
-class T_Information_Item_Tobeoperationalized_Memor_TimeseriesListView(generic.ListView):
+class T_Information_Item_Tobeoperationalized_Memor_TimeseriesListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_information_item_tobeoperationalized_memor_timeseries."""
     model = T_Information_Item_Tobeoperationalized_Memor_Timeseries
     paginate_by = 10
 
-class T_Information_Item_Tobeoperationalized_Memor_TimeseriesDetailView(generic.DetailView):
+class T_Information_Item_Tobeoperationalized_Memor_TimeseriesDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_information_item_tobeoperationalized_memor_timeseries."""
     model = T_Information_Item_Tobeoperationalized_Memor_Timeseries
 
 
-class T_Information_Item_Tobeoperationalized_Memor_Timeseries_ActListView(generic.ListView):
+class T_Information_Item_Tobeoperationalized_Memor_Timeseries_ActListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_information_item_tobeoperationalized_memor_timeseries_act."""
     model = T_Information_Item_Tobeoperationalized_Memor_Timeseries_Act
     paginate_by = 10
 
-class T_Information_Item_Tobeoperationalized_Memor_Timeseries_ActDetailView(generic.DetailView):
+class T_Information_Item_Tobeoperationalized_Memor_Timeseries_ActDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_information_item_tobeoperationalized_memor_timeseries_act."""
     model = T_Information_Item_Tobeoperationalized_Memor_Timeseries_Act
 
 
-class T_Category_TableListView(generic.ListView):
+class T_Category_TableListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_category_tables."""
     model = T_Category_Table
     paginate_by = 10
 
-class T_Category_TableDetailView(generic.DetailView):
+class T_Category_TableDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_category_table."""
     model = T_Category_Table
 
 
-class T_Category_Table_EntryListView(generic.ListView):
+class T_Category_Table_EntryListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_category_table_entries."""
     model = T_Category_Table_Entry
     paginate_by = 10
 
-class T_Category_Table_EntryDetailView(generic.DetailView):
+class T_Category_Table_EntryDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_category_table_entry."""
     model = T_Category_Table_Entry
 
 
-class T_Category_Table_Predicate_AsverbListView(generic.ListView):
+class T_Category_Table_Predicate_AsverbListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_category_table_predicates_asverb."""
     model = T_Category_Table_Predicate_Asverb
     paginate_by = 10
 
-class T_Category_Table_Predicate_AsverbDetailView(generic.DetailView):
+class T_Category_Table_Predicate_AsverbDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_category_table_predicate_asverb."""
     model = T_Category_Table_Predicate_Asverb
 
 
-class T_Category_TimeseriesListView(generic.ListView):
+class T_Category_TimeseriesListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_category_timeseries."""
     model = T_Category_Timeseries
     paginate_by = 10
 
-class T_Category_TimeseriesDetailView(generic.DetailView):
+class T_Category_TimeseriesDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_category_timeseries."""
     model = T_Category_Timeseries
 
 
-class T_Memorization_Package_Memory_Palace_Or_Cards_TechniqueListView(generic.ListView):
+class T_Memorization_Package_Memory_Palace_Or_Cards_TechniqueListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_memorization_packages_memory_palace_or_cards_technique."""
     model = T_Memorization_Package_Memory_Palace_Or_Cards_Technique
     paginate_by = 10
 
 
-class T_Memorization_Package_Memory_Palace_Or_Cards_TechniqueDetailView(generic.DetailView):
+class T_Memorization_Package_Memory_Palace_Or_Cards_TechniqueDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_memorization_package_memory_palace_or_cards_technique."""
     model = T_Memorization_Package_Memory_Palace_Or_Cards_Technique
 
 
-class T_Memory_Palace_TypeListView(generic.ListView):
+class T_Memory_Palace_TypeListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_memory_palaces_type."""
     model = T_Memory_Palace_Type
     paginate_by = 10
 
-class T_Memory_Palace_TypeDetailView(generic.DetailView):
+class T_Memory_Palace_TypeDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_memory_palace_type."""
     model = T_Memory_Palace_Type
 
 
-class T_Memory_Palace_Type_LocationListView(generic.ListView):
+class T_Memory_Palace_Type_LocationListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_memory_palaces_type_location."""
     model = T_Memory_Palace_Type_Location
     paginate_by = 10
 
-class T_Memory_Palace_Type_LocationDetailView(generic.DetailView):
+class T_Memory_Palace_Type_LocationDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_memory_palace_type_location."""
     model = T_Memory_Palace_Type_Location
 
 
-class T_Memory_Palace_Type_Location_Packageassignment_TimeseriesListView(generic.ListView):
+class T_Memory_Palace_Type_Location_Packageassignment_TimeseriesListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_memory_palaces_type_location."""
     model = T_Memory_Palace_Type_Location_Packageassignment_Timeseries
     paginate_by = 10
 
-class T_Memory_Palace_Type_Location_Packageassignment_TimeseriesDetailView(generic.DetailView):
+class T_Memory_Palace_Type_Location_Packageassignment_TimeseriesDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_memory_palace_type_location_packageassignment_timeseries."""
     model = T_Memory_Palace_Type_Location_Packageassignment_Timeseries
 
 
-class T_Memory_Palace_Type_Location_NumberListView(generic.ListView):
+class T_Memory_Palace_Type_Location_NumberListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_memory_palace_type_location_number."""
     model = T_Memory_Palace_Type_Location_Number
     paginate_by = 10
 
-class T_Memory_Palace_Type_Location_NumberDetailView(generic.DetailView):
+class T_Memory_Palace_Type_Location_NumberDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_memory_palace_type_location_number."""
     model = T_Memory_Palace_Type_Location_Number
 
 
-class T_Memory_Palace_Type_Location_DaytimeListView(generic.ListView):
+class T_Memory_Palace_Type_Location_DaytimeListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_memory_palaces_type_location_daytime."""
     model = T_Memory_Palace_Type_Location_Daytime
     paginate_by = 10
 
-class T_Memory_Palace_Type_Location_DaytimeDetailView(generic.DetailView):
+class T_Memory_Palace_Type_Location_DaytimeDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_memory_palace_type_location_daytime."""
     model = T_Memory_Palace_Type_Location_Daytime
 
 
-class T_Memory_Palace_Or_Cards_Memorization_TimeseriesListView(generic.ListView):
+class T_Memory_Palace_Or_Cards_Memorization_TimeseriesListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_memory_palaces_or_cards_memorization_timeseries."""
     model = T_Memory_Palace_Or_Cards_Memorization_Timeseries
     paginate_by = 10
 
-class T_Memory_Palace_Or_Cards_Memorization_TimeseriesDetailView(generic.DetailView):
+class T_Memory_Palace_Or_Cards_Memorization_TimeseriesDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_memory_palace_or_cards_memorization_timeseries."""
     model = T_Memory_Palace_Or_Cards_Memorization_Timeseries
 
 
-class T_Memory_Palace_Or_Cards_Memorization_Timeseries_ActionListView(generic.ListView):
+class T_Memory_Palace_Or_Cards_Memorization_Timeseries_ActionListView(LoginRequiredMixin, generic .ListView):
     """Generic class-based view for a list of t_memory_palace_or_cards_memorization_timeseries_actions."""
     model = T_Memory_Palace_Or_Cards_Memorization_Timeseries_Action
     paginate_by = 10
 
-class T_Memory_Palace_Or_Cards_Memorization_Timeseries_ActionDetailView(generic.DetailView):
+class T_Memory_Palace_Or_Cards_Memorization_Timeseries_ActionDetailView(LoginRequiredMixin, generic .DetailView):
     """Generic class-based detail view for a t_memory_palace_or_cards_memorization_timeseries_action."""
     model = T_Memory_Palace_Or_Cards_Memorization_Timeseries_Action
 
 
 
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
