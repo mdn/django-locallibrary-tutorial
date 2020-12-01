33a0465ec890bcc38f211cb5e4bf3d0a7baee41f
Hamish Willee <hamishwillee@gmail.com>
2020-12-01 17:38:02 +1100

    settings.py: Change templates dir match docs
    Note, this is kind of an irrelevant change. Just easier to track.

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./templates',],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
