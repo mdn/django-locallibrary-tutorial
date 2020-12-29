02132ec8edf86af30fd86dc6ad26066810ad5a5f
Hamish Willee <hamishwillee@gmail.com>
2020-12-28 21:50:02 -0800

    Prevent book being deleted until all its instances are deleted (#86)
    * Cascade book deletion
    
    * Restrict deletion of book

--- catalog/models.py
+++ catalog/models.py
@@ -71,7 +71,7 @@ class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4,
                           help_text="Unique ID for this particular book across whole library")
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
     borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
