01663f5b94841f9fc5a602b2515fac3b149b8f8f
claytoncook12 <claytoncook12@gmail.com>
2022-02-10 10:20:56 -0500

    update ALLOWED_HOST to `*`

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 # Set hosts to allow any app on Heroku and the local testing URL
-ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1',"*"]
 
 
 # Application definition
