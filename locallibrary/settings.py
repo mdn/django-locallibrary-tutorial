a6b2a44c68aae5b4ddeeed5b3ca6d4086f275e00
Hamish Willee <hamishwillee@gmail.com>
2022-10-11 14:41:08 +1100

    Add railway to allowed hosts and csrf

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,8 +28,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-# Set hosts to allow any app on Heroku and the local testing URL
-ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
+# Set hosts to allow any app on Railway and the local testing URL
+ALLOWED_HOSTS = ['.railway.app','127.0.0.1']
+
+# Set CSRF trusted origins to allow any app on Railway and the local testing URL
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app','https://*.127.0.0.1']
 
 
 # Application definition
