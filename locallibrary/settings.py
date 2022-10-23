549c67534593ebb8c51035628dfe0f8ae98f91a1
Hamish Willee <hamishwillee@gmail.com>
2022-10-24 10:59:10 +1100

    Add back secret key commented out (not needed but better fits instructions)

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -24,6 +24,7 @@ import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+#SECRET_KEY = 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag'
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
