38680cae10f875d92c37a5eeb3e851bb204682bd
Hamish Willee <hamishwillee@gmail.com>
2023-02-13 15:07:43 +1100

    Fix long line to make rendering better

--- catalog/views.py
+++ catalog/views.py
@@ -63,7 +63,11 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 10
 
     def get_queryset(self):
-        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+        return (
+            BookInstance.objects.filter(borrower=self.request.user)
+            .filter(status__exact='o')
+            .order_by('due_back')
+        )
 
 
 # Added as part of challenge!
