dc87cb82b8bdf0227d483e7b0633470cba4223cd
Hamish Willee <hamishwillee@gmail.com>
2023-07-28 16:58:31 +1000

    Minimal changes for 4.2 - inc use STORAGES

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -1,7 +1,8 @@
-"""locallibrary URL Configuration
+"""
+URL configuration for locallibrary project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,8 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
 from django.contrib import admin
+from django.urls import path
 
 # Use include() to add URLS from the catalog application and authentication system
 from django.urls import include
