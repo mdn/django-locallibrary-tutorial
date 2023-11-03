f7d5fb8459c3b104c57da7ca54a029f761f70dae
Hamish Willee <hamishwillee@gmail.com>
2023-11-03 15:18:15 +1100

    User instead of UserModel, following consistent pattern for model naming
    Co-authored-by: Özgür <ozgurturkiye@gmail.com>

--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -54,14 +54,14 @@ from django.contrib.auth.models import User  # Required to assign User as a borr
 
 # Get user model from settings
 from django.contrib.auth import get_user_model
-UserModel = get_user_model()
+User = get_user_model()
 
 class LoanedBookInstancesByUserListViewTest(TestCase):
 
     def setUp(self):
         # Create two users
-        test_user1 = UserModel.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
-        test_user2 = UserModel.objects.create_user(username='testuser2', password='2HJ1vRV0Z&3iD')
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        test_user2 = User.objects.create_user(username='testuser2', password='2HJ1vRV0Z&3iD')
 
         test_user1.save()
         test_user2.save()
@@ -197,10 +197,12 @@ class RenewBookInstancesViewTest(TestCase):
 
     def setUp(self):
         # Create a user
-        test_user1 = UserModel.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+
         test_user1.save()
 
-        test_user2 = UserModel.objects.create_user(username='testuser2', password='2HJ1vRV0Z&3iD')
+        test_user2 = User.objects.create_user(username='testuser2', password='2HJ1vRV0Z&3iD')
+
         test_user2.save()
         permission = Permission.objects.get(name='Set book as returned')
         test_user2.user_permissions.add(permission)
@@ -308,8 +310,8 @@ class AuthorCreateViewTest(TestCase):
 
     def setUp(self):
         # Create a user
-        test_user1 = UserModel.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
-        test_user2 = UserModel.objects.create_user(username='testuser2', password='2HJ1vRV0Z&3iD')
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        test_user2 = User.objects.create_user(username='testuser2', password='2HJ1vRV0Z&3iD')
 
         test_user1.save()
         test_user2.save()
