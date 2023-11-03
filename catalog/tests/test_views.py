39a162a1b778de6830d2d860935ebe9471e183fe
Hamish Willee <hamishwillee@gmail.com>
2023-11-03 15:20:53 +1100

    Update catalog/tests/test_views.py

--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -52,7 +52,6 @@ from django.utils import timezone
 from catalog.models import BookInstance, Book, Genre, Language
 
 # Get user model from settings
-
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
