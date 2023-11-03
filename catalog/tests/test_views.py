ef7d7a993acf0ebf98838f668b274eba003d8d97
Hamish Willee <hamishwillee@gmail.com>
2023-11-03 15:19:01 +1100

    Remove spurious lines

--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -198,11 +198,9 @@ class RenewBookInstancesViewTest(TestCase):
     def setUp(self):
         # Create a user
         test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
-
         test_user1.save()
 
         test_user2 = User.objects.create_user(username='testuser2', password='2HJ1vRV0Z&3iD')
-
         test_user2.save()
         permission = Permission.objects.get(name='Set book as returned')
         test_user2.user_permissions.add(permission)
