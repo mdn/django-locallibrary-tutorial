ceb41740cb8b1b08e8361d6a99770d4239653c10
Hamish Willee <hamishwillee@gmail.com>
2024-02-19 16:28:52 +1100

    Add pythonanywhere to ALLOWED_HOSTS, CSRF_TRUSTED_ORIGINS

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 # Set hosts to allow any app on Railway and the local testing URL
-ALLOWED_HOSTS = ['.railway.app','127.0.0.1']
+ALLOWED_HOSTS = ['.railway.app','.pythonanywhere.com','127.0.0.1']
 
 # Set CSRF trusted origins to allow any app on Railway and the local testing URL
-CSRF_TRUSTED_ORIGINS = ['https://*.railway.app','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app','https://*.pythonanywhere.com','https://*.127.0.0.1']
 
 
 # Application definition
