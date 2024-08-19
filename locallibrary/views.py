cbc1275f96368b9ca7524ce3231da82890a66b7c
Hamish Willee <hamishwillee@gmail.com>
2024-08-19 12:34:42 +1000

    Signup password

--- locallibrary/views.py
+++ locallibrary/views.py
@@ -1,14 +1,14 @@
 
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from locallibrary.forms import PasswordUserCreationForm
 from django.contrib.auth import login
 
 
 def register_user(request):
     if request.method != 'POST':
-        form = UserCreationForm()
+        form = PasswordUserCreationForm()
     else:
-        form = UserCreationForm(request.POST)
+        form = PasswordUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
